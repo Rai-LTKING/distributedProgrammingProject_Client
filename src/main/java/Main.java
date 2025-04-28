53103b16fe44c5146bf524150b4aeed71e24d17c
LeTiep <letiepltking@gmail.com>
2025-04-28 15:44:16 +0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import com.formdev.flatlaf.FlatLightLaf;
+import controller.LoginController;
+import controller.MainController;
+import entity.Employee;
+import service.inteface.AllService;
+import view.frame.iframe.ILoginFrame;
+import view.frame.imp.LoginFrameImp;
+import view.frame.imp.MainFrameImp;
+
+
+import java.rmi.Naming;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllService allService = null;
+
+        try {
+             allService = (AllService) Naming.lookup("rmi://DESKTOP-KITCULR:7102/allService");
+            System.out.println("Successfully connected to RMI service: " + allService);
+        } catch (Exception e) {
+            System.err.println("Failed to connect to RMI service: " + e.getMessage());
+            e.printStackTrace();
+        }
+        FlatLightLaf.setup();
+        ILoginFrame loginFrame = new LoginFrameImp();
+        LoginController loginController = new LoginController(loginFrame,
+                allService.getAccountService());
+        loginController.show();
+
+    }
+}
